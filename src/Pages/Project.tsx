e0a3eccbcba1ccc4b626eb06b23a25280dc370e4
amanda almeida <manda.fonsecaff@gmail.com>
2023-03-27 22:34:52 -0300

    feat(Projetos): Adiciona alguns projetos e hover nos links do header

--- src/Pages/Project.tsx
+++ src/Pages/Project.tsx
@@ -1,6 +1,6 @@
 import { CardProject } from "../components/cardProject";
 import image from "../assets/images/Project/image1.png";
-import PontosTurísticos from "../assets/images/Project/Pontos-Turisticos.png";
+import PontosTurísticos from "../assets/images/Project/PontosTuristicos.png";
 import Git from "../assets/images/Project/Git.png";
 import Agenda from "../assets/images/Project/Agenda.png";
 import Spotify from "../assets/images/Project/Spotify.png";
